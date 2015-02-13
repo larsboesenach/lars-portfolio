e0f64dd9c58fc0ecaad88f0c4a03350b99eaaa1a
Lars Boesenach <lars@lokaalgevonden.nl>
2015-02-13 21:23:19 +0100

    Added a bouncing animation on the header profile img box thingy

--- includes/home-header.php
+++ includes/home-header.php
@@ -2,7 +2,15 @@
 	<div class="container">
 
 		<div class="header">
-			
+			<div class="Profile-wrap">
+
+				<div class="Profile-img">
+					<div class="img-centering-wrap">
+						<div class="img-profile animation-target"></div>
+					</div>
+				</div>
+
+			</div>
 		</div>
 
 	</div>
